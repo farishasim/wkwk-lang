e1f057f0df674de0698cf92f46a32d1ac9d91fa6
Faris Hasim <farishasimsyauqi@gmail.com>
2021-06-23 22:58:44 +0700

    update interpreter

--- Makefile
+++ Makefile
@@ -2,11 +2,17 @@ CXX = g++
 DRIVER = driver
 BIN = bin
 VM = ./vm
+INTERPRETER = boi
 
-compile: compile_vm
+compile: compile_boi
 
-compile_vm:
+driver:
 	cd ${VM}; \
 	${CXX} -c *.cpp; \
 	${CXX} -o ${DRIVER} *.o; \
 	rm *.o; \
+
+compile_boi: vm/VM.o boi.o
+	mv vm/VM.o .; \
+	${CXX} -o ${INTERPRETER} *.o; \
+	rm *.o \
---
+++ boi.cpp
@@ -0,0 +1,72 @@
+#include "./vm/VM.hpp"
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+byte * program;
+
+byte get8bit(char * str) {
+    int i;
+    byte newbyte = 0;
+    for (i = 0; i<8; i++) {
+        newbyte<<=1;
+        if (str[i] == 'k') {
+            newbyte += 1;
+        } else if (str[i] == 'w') {
+            
+        } else {
+            cout << "Unresolved Symbol: " << str[i] << "." << endl;
+            exit(0);
+        }
+    }
+    return newbyte;
+}
+
+void interpret(char * filename) {
+    char * buffer;
+    int len;
+
+    // load program
+    ifstream infile;
+    infile.open(filename);
+    infile.seekg(0, ios::end);
+    len = infile.tellg();
+    infile.seekg(0, ios::beg);
+    // delete[] buffer;
+    buffer = new char[len];
+    infile.read(buffer, len);
+    infile.close();
+
+    if (len % 8 != 0) {
+        cout << "Invalid code length.\n";
+        exit(0);
+    }
+
+    int codelen = len / 8;
+    byte * code = new byte[codelen];
+
+    for(int i=0; i<codelen; i++) {
+        code[i] = get8bit(&buffer[i*8]);
+    }
+
+    delete[] buffer;
+    program = code;
+}
+
+int main(int argc, char ** args) {
+
+    if (argc < 2)  {
+        cout << "usage : ./boi filename\n";
+        exit(0);
+    }
+    // cout << "sampaisini\n";
+    // loadProgram(args[1]);
+    interpret(args[1]);
+
+    VM vm(program);
+
+    vm.run();
+
+    return 0;
+}
